cc8c221ffaf125e569875d65b5dd6c88f10517c8
Hossein Alipour <hossein.4lipour@gmail.com>
2021-09-26 21:13:34 +0300

    create crud example

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { Client } = require("pg");
+const client = new Client();
+client.connect();
+
 const port = 3000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ result: "Hello World!" });
+//read
+app.get("/", async (req, res) => {
+  res = await client.query("SELECT now() as now");
+  res.json({ result: res });
+});
+
+// create
+app.post("/", async (req, res) => {
+  res = await client.query("insert into ");
+  res.statusCode(201);
+});
+
+// update
+app.put("/", async (req, res) => {
+  res = await client.query("update  ");
+  res.statusCode(200);
+});
+
+//delete
+app.delete("/", async (req, res) => {
+  res = await client.query("delete  ");
+  res.statusCode(200);
 });
 
 app.listen(port, () => {
